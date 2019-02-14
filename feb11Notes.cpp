7e091181791c4003be8258c850d217981af9ed32
IronHeart7334 <greengrappler12@gmail.com>
2019-02-13 17:07:43 -0800

    awesome recursive print queue

--- feb11Notes.cpp
+++ feb11Notes.cpp
@@ -22,26 +22,32 @@ void printQueue(q* rear);
 
 int main(){
     //for stack, need to pass top as node*&
+    frame* top = 0;
 
-    frame* top = new frame;
-    top->next = 0;
-    top->value = 0;
-
-    for(int i = 1; i <= 5; i++){
+    for(int i = 0; i < 4; i++){
+        cout << "Pushing " << i << endl;
         push(top, i);
+        printFrame(top);
     }
-    printFrame(top);
-    cout << "Top is " << pop(top)->value << endl;
-    printFrame(top);
 
-    q* rear = new q;
-    rear->prev = 0;
-    rear->value = 0;
+    while(top){
+        cout << "Pop top " << pop(top)->value << endl;
+        printFrame(top);
+    }
 
-    for(int i = 5; i < 25; i+=5){
+    //###queues#####################################################
+
+    q* rear = 0; //can have 0 element queue
+
+    for(int i = 0; i < 5; i++){
         enqueue(rear, i);
     }
     printQueue(rear);
+
+    while(rear){
+        cout << "Dequeing " << dequeue(rear)->value << endl;
+        printQueue(rear);
+    }
 
     return 0;
 }
@@ -56,17 +62,18 @@ void push(frame* &top, int val){
 frame* pop(frame* &top){
     frame* ret = top;
     if(top->next){
-        //prevent deleting base
-        frame* ret = top;
         top = top->next;
+        ret->next = 0;
+    } else {
+        top = 0;
     }
-    return ret; //can't do return top
+    return ret;
 }
 
 void printFrame(frame* top){
     frame* current = top;
-    while(current->next){
-        //this way it won't print the base
+
+    while(current){
         cout << current->value << endl;
         current = current->next;
     }
@@ -78,23 +85,41 @@ void enqueue(q* &rear, int val){
     newRear->prev = rear;
     rear = newRear;
 }
+
 q* dequeue(q* &rear){
     q* ret = rear;
+    q* current = rear;
 
-    if(ret->prev){
-        //don't delete rear?
+    if(!rear->prev){
+        ret = rear;
+        rear = 0;
+    }
 
+    while(current->prev){
+        if(current->prev->prev == 0){
+            ret = current->prev;
+            current->prev = 0;
+        } else {
+            current = current->prev;
+        }
     }
 
     return ret;
 }
 void printQueue(q* rear){
+    /*
     q* current = rear;
     cout << "LAST" << endl;
-    while(current->prev){
-        //skip head
+    while(current){
         cout << current->value << endl;
         current = current->prev;
     }
     cout << "FIRST" << endl;
+    */
+    if(rear){
+        if(rear->prev){
+            printQueue(rear->prev);
+        }
+        cout << rear->value << " <- ";
+    }
 }
