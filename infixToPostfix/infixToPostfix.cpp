948065446e44d5ec477b0165e0a53838912ff561
IronHeart7334 <greengrappler12@gmail.com>
2019-02-18 10:16:20 -0800

    works so far, but a bit messy

--- infixToPostfix/infixToPostfix.cpp
+++ infixToPostfix/infixToPostfix.cpp
@@ -3,6 +3,85 @@
 
 using namespace std;
 
+struct frame{
+    frame* next;
+    char value;
+};
+
+void push(frame* &top, char val);
+frame* pop(frame* &top);
+void printFrame(frame* top);
+
 int main(){
+    fstream file = fstream("input.txt");
+    char token;
+    frame* operators = 0;
+    string result = "";
+
+    while(file >> token){
+        cout << token;
+        if(isdigit(token)){
+            result += token;
+        } else {
+            switch(token){
+            case '(':
+                push(operators, token);
+                break;
+            case ')':
+                while(token != '('){
+                    token = pop(operators)->value;
+                    if(token != '('){
+                        result += token;
+                    }
+                }
+                break;
+            case '+':
+                while(operators && operators->value != '('){
+                    result += pop(operators)->value;
+                }
+                push(operators, token);
+                break;
+            case '-':
+                while(operators && operators->value != '('){
+                    result += pop(operators)->value;
+                }
+                push(operators, token);
+                break;
+            }
+        }
+    }
+    file.close();
+    while(operators){
+        result += pop(operators)->value;
+    }
+    cout << endl << result << endl;
+
     return 0;
+}
+
+void push(frame* &top, char val){
+    frame* newFrame = new frame;
+    newFrame->next = top;
+    newFrame->value = val;
+    top = newFrame;
+}
+
+frame* pop(frame* &top){
+    frame* ret = top;
+    if(top->next){
+        top = top->next;
+        ret->next = 0;
+    } else {
+        top = 0;
+    }
+    return ret;
+}
+
+void printFrame(frame* top){
+    frame* current = top;
+
+    while(current){
+        cout << current->value << endl;
+        current = current->next;
+    }
 }
