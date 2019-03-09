5c0a3d2ec7967de033a8f1db7ab55384cfb94f0b
IronHeart7334 <greengrappler12@gmail.com>
2019-03-09 08:21:03 -0800

    done with searchSort

--- Exam1/searchSort.cpp
+++ Exam1/searchSort.cpp
@@ -7,8 +7,12 @@ const int COLS = 4;
 void reset(int a[ROWS][COLS]);
 
 void bubbleSort(int a[ROWS][COLS], int col, bool ascending);
+void gappedSort(int a[ROWS][COLS], int col, bool ascending, int gap);
+void shellSort(int a[ROWS][COLS], int col, bool ascending);
+void insertionSort(int a[ROWS][COLS], int col, bool ascending);
 void selectSort(int a[ROWS][COLS], int col, bool ascending);
 
+int binarySearch(int a[ROWS][COLS], int col, int searchFor);
 
 int useSearchSort();
 void switchRows(int arr[ROWS][COLS], int a, int b);
@@ -19,8 +23,11 @@ void print(int a[ROWS][COLS]);
 
 int useSearchSort(){
     int a[5][4];
+    int ip;
+    int idx;
 
     reset(a);
+    cout << "Original" << endl;
     print(a);
     cout << "Bubble sort" << endl;
     bubbleSort(a, 0, true);
@@ -29,7 +36,26 @@ int useSearchSort(){
     cout << "Select sort" << endl;
     selectSort(a, 1, false);
     print(a);
+    reset(a);
+    cout << "Shell sort" << endl;
+    shellSort(a, 2, true);
+    print(a);
+    reset(a);
 
+    cout << "Insertion sort" << endl;
+    insertionSort(a, 3, true);
+    print(a);
+
+    cout << "Last column: ";
+    for(int i = 0; i < ROWS; i++){
+        cout << a[i][3] << " ";
+    }
+    cout << endl;
+
+    cout << "Enter a number to search for in the last column: ";
+    cin >> ip;
+    idx = binarySearch(a, 3, ip);
+    cout << ip << ((idx == -1) ? " is not in the last column." : " is located at index " + idx) << endl;
     return 0;
 }
 
@@ -42,7 +68,6 @@ void reset(int a[ROWS][COLS]){
 }
 
 void bubbleSort(int a[ROWS][COLS], int col, bool ascending){
-    int temp[COLS];
     for(int i = 0; i < ROWS - 1; i++){
         for(int j = 0; j < ROWS - i - 1; j++){
             if(
@@ -54,6 +79,26 @@ void bubbleSort(int a[ROWS][COLS], int col, bool ascending){
         }
 
     }
+}
+
+void gappedSort(int a[ROWS][COLS], int col, bool ascending, int gap){
+    for( ; gap >= 1; gap /= 2){
+        for(int i = gap; i < ROWS; i++){
+            for(int j = i; j >= gap && a[(ascending) ? j - gap : j][col] > a[(ascending) ? j : j - gap][col]; j -= gap){
+                switchRows(a, j - gap, j);
+                //print(a);
+                //uncomment this to see it in action
+            }
+        }
+    }
+}
+
+void shellSort(int a[ROWS][COLS], int col, bool ascending){
+    gappedSort(a, col, ascending, ROWS / 2);
+}
+
+void insertionSort(int a[ROWS][COLS], int col, bool ascending){
+    gappedSort(a, col, ascending, 1);
 }
 
 void selectSort(int a[ROWS][COLS], int col, bool ascending){
@@ -70,6 +115,27 @@ void selectSort(int a[ROWS][COLS], int col, bool ascending){
         }
         switchRows(a, idxOfSmallest, i);
     }
+}
+
+//only works for ascending order
+int binarySearch(int a[ROWS][COLS], int col, int searchFor){
+    int ret = -1;
+    int minIdx = 0;
+    int maxIdx = ROWS - 1;
+    int midIdx;
+
+    while(minIdx <= maxIdx && ret == -1){
+        midIdx = (maxIdx + minIdx) / 2;
+        if(a[midIdx][col] == searchFor){
+            ret = midIdx;
+        } else if(a[midIdx][col] < searchFor){
+            minIdx = midIdx + 1;
+        } else {
+            maxIdx = midIdx - 1;
+        }
+    }
+
+    return ret;
 }
 
 void switchRows(int arr[ROWS][COLS], int a, int b){
