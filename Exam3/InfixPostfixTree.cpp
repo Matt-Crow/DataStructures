fc43f00d04ea9a64a30197e6df3b46b0eec83703
IronHeart7334 <greengrappler12@gmail.com>
2019-04-26 12:17:15 -0700

    done. I wish I had time to improve it though, such as converting input to postfix before evaluating

--- Exam3/InfixPostfixTree.cpp
+++ Exam3/InfixPostfixTree.cpp
@@ -15,29 +15,29 @@ struct frame{
 
 Tree* newTree(char data);
 
-void preOrder(Tree* root, ostream &output);
 void inOrder(Tree* root, ostream &output);
-int postOrder(Tree* root, ostream &output);
 
-void push(frame* &top, char val);
 void push(frame* &top, Tree* val);
+
 frame* pop(frame* &top);
+
 void printFrame(frame* top);
 
 int toInt(char c);
 char toChar(int i);
 
-//string toPostfix(string infix);
-//int evaluatePostfix(string postfix);
-int evaluateBinaryOp(Tree* root);
+int evaluate(Tree* root);
 
-//useInfixPostfixTree
-int main(){
+int useInfixPostfixTree(){
     frame* myStack = 0;
-    string ip = "12+3*";
+    string ip = "";
+
+    cout << "Enter a postfix expression: ";
+    cin >> ip;
+
     for(int i = 0; i < ip.size(); i++){
         if(isdigit(ip[i])){
-            push(myStack, ip[i]);
+            push(myStack, newTree(ip[i]));
         } else {
             Tree* right = pop(myStack)->value;
             Tree* left = pop(myStack)->value;
@@ -48,10 +48,9 @@ int main(){
             push(myStack, a);
         }
     }
+    cout << ip << " -> ";
     inOrder(myStack->value, cout);
-    cout << endl;
-    int val = postOrder(myStack->value, cout);
-    cout << endl << val << endl;
+    cout << " = " << evaluate(myStack->value) << endl;
 
     return 0;
 }
@@ -64,13 +63,6 @@ Tree* newTree(char data){
     return ret;
 }
 
-void preOrder(Tree* root, ostream &output){
-    if(root){
-        output << root->data << " ";
-        preOrder(root->leftChild, output);
-        preOrder(root->rightChild, output);
-    }
-}
 void inOrder(Tree* root, ostream &output){
     if(root){
         bool isLeaf = !(root->leftChild && root->rightChild);
@@ -85,38 +77,11 @@ void inOrder(Tree* root, ostream &output){
         }
     }
 }
-int postOrder(Tree* root, ostream &output){
-    int ret;
-    if(root){
-        if(!isdigit(root->data)){
-            ret += evaluateBinaryOp(root);
-            cout << "op ret is " << ret << endl;
-        }
-        postOrder(root->leftChild, output);
-        postOrder(root->rightChild, output);
-        output << root->data << " ";
-
-        if(isdigit(root->data)){
-            ret += toInt(root->data);
-            cout << "num ret is " << ret << endl;
-        }
-        return ret;
-    }
-    return 0;
-}
-
 
 void push(frame* &top, Tree* val){
     frame* newFrame = new frame;
     newFrame->next = top;
     newFrame->value = val;
-    top = newFrame;
-}
-
-void push(frame* &top, char val){
-    frame* newFrame = new frame;
-    newFrame->next = top;
-    newFrame->value = newTree(val);
     top = newFrame;
 }
 
@@ -183,143 +148,32 @@ char toChar(int i){
     return (char)(48 + i);
 }
 
-//change this to just evaluate
-int evaluateBinaryOp(Tree* root){
+int evaluate(Tree* root){
     int ret = 0;
 
     if(root){
         char binaryOp = root->data;
-        int left = evaluateBinaryOp(root->leftChild);
-        int right = evaluateBinaryOp(root->rightChild);
+        int left = evaluate(root->leftChild);
+        int right = evaluate(root->rightChild);
 
         switch(binaryOp){
         case '+':
-            ret = right + left;
+            ret = left + right;
             break;
         case '-':
-            ret = right - left;
+            ret = left - right;
             break;
         case '*':
-            ret = right * left;
+            ret = left * right;
             break;
         case '/':
-            ret = right / left;
+            ret = left / right;
             break;
         default:
+            //if it is not an operator, just return the value
             ret = toInt(binaryOp);
-            cout << "Invalid binaryOp: " + binaryOp << endl;
         }
-        cout << right << " " << binaryOp << " " << left << " = " << ret << endl;
     }
 
     return ret;
 }
-
-/*
-string toPostfix(string infix){
-    string result = "";
-    frame* ops = 0;    //opperator stack
-
-    for(char token : infix){
-        if(isdigit(token)){
-            result += token;
-        } else {
-            switch(token){
-            case '(':
-                //if it is an (, push it to the stack
-                push(ops, '(');
-                break;
-            case ')':
-                //if it is an ), pop until you hit an (
-                while(token != '('){
-                    token = pop(ops)->value;
-                    if(token != '('){
-                        result += token;
-                    }
-                }
-                break;
-            //if the token is an operator, pop until the top is less precedence than the token,
-            //then push to token.
-            case '+':
-                while(ops && ops->value != '('){
-                    result += pop(ops)->value;
-                }
-                push(ops, token);
-                break;
-            case '-':
-                while(ops && ops->value != '('){
-                    result += pop(ops)->value;
-                }
-                push(ops, token);
-                break;
-            case '*':
-                while(
-                    ops &&
-                    ops->value != '(' &&
-                    ops->value != '+' &&
-                    ops->value != '-'
-                ){
-                    result += pop(ops)->value;
-                }
-                push(ops, token);
-                break;
-            case '/':
-                while(
-                    ops &&
-                    ops->value != '(' &&
-                    ops->value != '+' &&
-                    ops->value != '-'
-                ){
-                    result += pop(ops)->value;
-                }
-                push(ops, token);
-                break;
-            case ' ':
-                //space
-                break;
-            default:
-                cout << "Not supported token: " << token << endl;
-            }
-        }
-    }
-    while(ops){
-        result += pop(ops)->value;
-    }
-
-    return result;
-}
-
-int evaluatePostfix(string postfix){
-    frame* nums = 0;
-    int a;
-    int b;
-
-    for(char token : postfix){
-        if(isdigit(token)){
-            push(nums, token);
-        } else {
-            a = toInt(pop(nums)->value);
-            b = toInt(pop(nums)->value);
-
-            switch(token){
-            case '+':
-                push(nums, toChar(a + b));
-                break;
-            case '-':
-                push(nums, toChar(b - a));
-                break;
-            case '*':
-                push(nums, toChar(a * b));
-                break;
-            case '/':
-                push(nums, toChar(b / a));
-                break;
-            default:
-                cout << "Unsupported token: " << token << endl;
-            }
-        }
-    }
-
-    return toInt(pop(nums)->value);
-}
-*/
