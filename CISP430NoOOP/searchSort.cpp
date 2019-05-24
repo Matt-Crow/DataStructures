b7c818f3ce11e34468196816c3347ff86fbe830d
IronHeart7334 <greengrappler12@gmail.com>
2019-05-24 12:32:42 -0700

    merge sort is done, but might want to include more output so it's clearer what it is doing

--- CISP430NoOOP/searchSort.cpp
+++ CISP430NoOOP/searchSort.cpp
@@ -10,6 +10,10 @@ void gappedSort(int a[], int length, int gapSize);
 void insertionSort(int a[], int length);
 void shellSort(int a[], int length);
 
+void quickSort(int a[], int start, int end);
+int partition(int a[], int start, int end);
+void mergeSort(int a[], int start, int end);
+
 int binarySearch(int a[], int length, int searchFor);
 
 
@@ -29,7 +33,9 @@ int useSearchSort(){
         cout << "3: use select sort" << endl;
         cout << "4: use insertion sort" << endl;
         cout << "5: use shell sort" << endl;
-        cout << "6: use binary search" << endl;
+        cout << "6: use quick sort" << endl;
+        cout << "7: use merge sort" << endl;
+        cout << "8: use binary search" << endl;
         cout << "-1: quit" << endl;
         cin >> ip;
 
@@ -66,6 +72,18 @@ int useSearchSort(){
             print(a, 10);
             break;
         case 6:
+            quickSort(a, 0, 10 - 1);
+            sorted = true;
+            cout << "After quick sort: ";
+            print(a, 10);
+            break;
+        case 7:
+            mergeSort(a, 0, 10 - 1);
+            sorted = true;
+            cout << "After merge sort: ";
+            print(a, 10);
+            break;
+        case 8:
             if(!sorted){
                 cout << "Array must be sorted to perform binary search" << endl;
                 break;
@@ -78,7 +96,7 @@ int useSearchSort(){
             } else {
                 cout << ip << " is not in the array" << endl;
             }
-            ip = 6;
+            ip = 8;
         }
     }while(ip >= 0);
 
@@ -169,6 +187,110 @@ This gap keeps shrinking with every pass.
 void shellSort(int a[], int length){
     gappedSort(a, length, length / 2);
 }
+
+
+void quickSort(int a[], int start, int end){
+    if(start < end){
+        int mid = partition(a, start, end);
+        quickSort(a, start, mid - 1);
+        quickSort(a, mid + 1, end);
+    }
+}
+
+/*
+Moves a[end], the PIVOT, to its proper place
+in the array, and sorts it so everything to
+its left is smaller than it, and everything
+to its right is larger.
+*/
+int partition(int a[], int start, int end){
+    int pivotValue = a[end];
+    int i = start;
+    int temp;
+    cout << "Pivot is " << pivotValue << endl;
+    for(int j = start; j < end; j++){
+        //cout << "Checking index " << j << endl;
+        if(a[j] <= pivotValue){
+            temp = a[j];
+            a[j] = a[i];
+            a[i] = temp;
+            i++;
+            print(a, 10);
+        }
+        //cout << "Pivot should be moved to " << i << endl;
+        //print(a, 10);
+    }
+    temp = a[i];
+    a[i] = a[end];
+    a[end] = temp;
+    print(a, 10);
+    return i;
+}
+
+
+/*
+Keep breaking the array in half until we only
+have two items to compare, the sort those two,
+sort that sorted set with the next sorted set of 2,
+creating a set of 4, sort that with the next set of 4, etc.
+*/
+void mergeSort(int a[], int start, int end){
+    if(start < end){
+        int mid = (start + end) / 2;
+        mergeSort(a, start, mid);
+        mergeSort(a, mid + 1, end);
+
+        //merge
+        //each split is technically a sorted array
+        int a1 = start;
+        int a2 = mid + 1;
+        int mergedArray[end - start] = {0};
+        int mergedArrayIdx = 0;
+        /*
+        cout << start << " < " << mid << " < " << end<< endl;
+        cout << "A1 is " << endl;
+        for(int i = start; i <= mid; i++){
+            cout << a[i] << " ";
+        }
+        cout << endl << "A2 is " << endl;
+        for(int i = mid + 1; i <= end; i++){
+            cout << a[i] << " ";
+        }
+        cout << endl;
+        */
+        while(a1 <= mid && a2 <= end){
+            if(a[a1] <= a[a2]){
+                mergedArray[mergedArrayIdx] = a[a1];
+                a1++;
+            } else {
+                mergedArray[mergedArrayIdx] = a[a2];
+                a2++;
+            }
+            mergedArrayIdx++;
+        }
+
+        //copy the rest in
+        while(a1 <= mid){
+            mergedArray[mergedArrayIdx] = a[a1];
+            a1++;
+            mergedArrayIdx++;
+        }
+        while(a2 <= end){
+            mergedArray[mergedArrayIdx] = a[a2];
+            a2++;
+            mergedArrayIdx++;
+        }
+
+        //copy new array into original
+        for(int i = start; i <= end; i++){
+            a[i] = mergedArray[i - start];
+        }
+
+        print(mergedArray, end - start + 1);
+    }
+}
+
+
 
 /*
 Starts at the middle, keeps cutting in half and choosing
