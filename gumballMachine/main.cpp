894cc0552d067bafdf4e737edaee2a5f1df0411c
IronHeart7334 <greengrappler12@gmail.com>
2019-10-26 13:30:11 -0700

    Looks like nickels work thus far

--- gumballMachine/main.cpp
+++ gumballMachine/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 enum Q{q0, q5, q10, q15, q20};
@@ -15,12 +16,40 @@ const string I_STRINGS[] = {"NICKEL", "DIME", "BUY", "QUIT"};
 
 const string PROMPT_STRING = "NICKEL, DIME, BUY, or QUIT: ";
 
+//begin Matt's code
+struct Transition
+{
+    Q newState;
+    O output;
+};
+Transition T(Q newState, O output); //constructor
+//end Matt's code
+
 int main()
 {
     Q q = q0;
     string input = "";
     string output = "";
     //begin Matt's code
+    Transition currTransition;
+    bool isTransitioning;
+    //initialize transition function,
+    //set all to invalid first
+    Transition tf[5][4] = {};
+    for(int i = q0; i <= q20; i++)
+    {
+        for(int j = NICKEL; j <= QUIT; j++)
+        {
+            tf[i][j] = T((Q)i, INVALID);
+        }
+    }
+
+    //all the nickel stuff
+    tf[q0][NICKEL] = T(q5, NONE);
+    tf[q5][NICKEL] = T(q10, NONE);
+    tf[q10][NICKEL] = T(q15, NONE);
+    tf[q15][NICKEL] = T(q20, NONE);
+    tf[q20][NICKEL] = T(q20, RETURN);
     //end Matt's code
 
     cout << PROMPT_STRING;
@@ -30,7 +59,21 @@ int main()
     {
 
         ///assign output and q based on current state and input
+        //begin Matt's code
+        isTransitioning = false;
+        if(input.compare("NICKEL") == 0){
+            currTransition = tf[q][NICKEL];
+            isTransitioning = true;
+        } else {
+            output = O_STRINGS[INVALID];
+        }
 
+        if(isTransitioning)
+        {
+            q = currTransition.newState;
+            output = O_STRINGS[currTransition.output];
+        }
+        //end Matt's code
         cout << output << endl;
         cout << "Current state: " << Q_STRINGS[q] << endl;
         cout << PROMPT_STRING;
@@ -39,3 +82,13 @@ int main()
 
     return 0;
 }
+
+//begin Matt's code
+Transition T(Q q, O o)
+{
+    Transition t;
+    t.newState = q;
+    t.output = o;
+    return t;
+}
+//end Matt's code
