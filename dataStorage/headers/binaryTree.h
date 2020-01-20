5423ca021e8be5df1bb7da7cb12209970af0d0ac
IronHeart7334 <greengrappler12@gmail.com>
2020-01-19 17:02:07 -0800

    cleaned up a bit to remove functions I've already implemented from old files

--- dataStorage/headers/binaryTree.h
+++ dataStorage/headers/binaryTree.h
@@ -1,42 +1,22 @@
+/*
+This file is being destroyed
 #ifndef BINARY_TREE
 #define BINARY_TREE
-/*
+
 template<class T>
 class TreeNode{
     public:
-        TreeNode(T val);
-        ~TreeNode();
-        void cascadeDelete();
-        void setLeft(TreeNode<T>* left);
-        void setRight(TreeNode<T>* right);
-        TreeNode<T>* getLeft();
-        TreeNode<T>* getRight();
-        T getValue();
         int getHeight();
         int getArraySize();
         void populateArray(T* a, int aLen, int idx);
-        void inOrder();
-    private:
-        T value;
-        TreeNode<T>* left;
-        TreeNode<T>* right;
 };
 
 template<class T>
 class BinaryTree{
     public:
-        BinaryTree();
-        ~BinaryTree();
-        void put(T val);
-        bool isEmpty();
         T* toArray();
         int getArraySize();
-        void print();
-        static int test();
-    private:
-        TreeNode<T>* root;
 };
 
-template class BinaryTree<int>;
-*/
 #endif
+*/
--- dataStorage/src/binaryTree.cpp
+++ dataStorage/src/binaryTree.cpp
@@ -1,49 +1,5 @@
-#include <iostream>
-#include "binaryTree.h"
-
-template<class T>
-TreeNode<T>::TreeNode(T val){
-    this->value = val;
-    this->left = 0;
-    this->right = 0;
-}
-
-template<class T>
-TreeNode<T>::~TreeNode(){
-    std::cout << "Delete " << this->value << std::endl;
-}
-
-template<class T>
-void TreeNode<T>::cascadeDelete(){
-    if(this->left){
-        this->left->cascadeDelete();
-    }
-    if(this->right){
-        this->right->cascadeDelete();
-    }
-    delete this;
-}
-
-template<class T>
-void TreeNode<T>::setLeft(TreeNode<T>* left){
-    this->left = left;
-}
-
-template<class T>
-void TreeNode<T>::setRight(TreeNode<T>* right){
-    this->right = right;
-}
-
-template<class T>
-TreeNode<T>* TreeNode<T>::getLeft(){
-    return this->left;
-}
-
-template<class T>
-TreeNode<T>* TreeNode<T>::getRight(){
-    return this->right;
-}
-
+//this file is currently being destroyed
+/*
 template<class T>
 int TreeNode<T>::getHeight(){
     int left = (this->left) ? this->left->getHeight() + 1 : 0;
@@ -77,70 +33,6 @@ void TreeNode<T>::populateArray(T* a, int aLen, int idx){
 }
 
 template<class T>
-T TreeNode<T>::getValue(){
-    return this->value;
-}
-
-template<class T>
-void TreeNode<T>::inOrder(){
-    if(this->left){
-        this->left->inOrder();
-    }
-    std::cout << this->value << " ";
-    if(this->right){
-        this->right->inOrder();
-    }
-}
-
-
-
-template<class T>
-BinaryTree<T>::BinaryTree(){
-    this->root = 0;
-}
-
-template<class T>
-BinaryTree<T>::~BinaryTree(){
-    if(this->root){
-        this->root->cascadeDelete();
-        this->root = 0;
-    }
-}
-
-template<class T>
-void BinaryTree<T>::put(T val){
-    TreeNode<T>* nn = new TreeNode<T>(val);
-    if(this->isEmpty()){
-        this->root = nn;
-    } else {
-        TreeNode<T>* curr = this->root;
-        bool done = false;
-        while(!done){
-            if(curr->getValue() > val){
-                if(curr->getLeft()){
-                    curr = curr->getLeft();
-                } else {
-                    curr->setLeft(nn);
-                    done = true;
-                }
-            } else {
-                if(curr->getRight()){
-                    curr = curr->getRight();
-                } else {
-                    curr->setRight(nn);
-                    done = true;
-                }
-            }
-        }
-    }
-}
-
-template<class T>
-bool BinaryTree<T>::isEmpty(){
-    return this->root == 0;
-}
-
-template<class T>
 T* BinaryTree<T>::toArray(){
     T* ret = 0;
     if(!this->isEmpty()){
@@ -157,35 +49,16 @@ int BinaryTree<T>::getArraySize(){
 }
 
 template<class T>
-void BinaryTree<T>::print(){
-    if(!this->isEmpty()){
-        this->root->inOrder();
-    }
-}
-
-template<class T>
 int BinaryTree<T>::test(){
     BinaryTree<int>* tree = new BinaryTree<int>();
     int ip = 0;
 
     do {
-        std::cout << "0: Print the binary tree" << std::endl;
-        std::cout << "1: Insert into the binary tree" << std::endl;
         std::cout << "2: Convert the binary tree to an array" << std::endl;
         std::cout << "-1: Quit" << std::endl;
         std::cout << "Choose an option:" << std::endl;
         std::cin >> ip;
         switch(ip){
-            case 0:
-                tree->print();
-                std::cout << std::endl;
-                break;
-            case 1:
-                std::cout << "Enter a value to insert: ";
-                std::cin >> ip;
-                tree->put(ip);
-                ip = 1;
-                break;
             case 2:
                 int* a = tree->toArray();
                 int len = tree->getArraySize();
@@ -204,3 +77,4 @@ int BinaryTree<T>::test(){
 
     return 0;
 }
+*/
