f34b6869e7a1c864481b36e28194f7c7936f67cc
IronHeart7334 <greengrappler12@gmail.com>
2020-08-23 12:24:05 -0700

    Using radix sort doesn't make a lot of sense when sorting numbers by their digits, as why not just use a simpler / faster algorithm? But what if we interprate fields of a struct as 'digits'?

--- sorting/headers/radixPrioritySort.h
+++ sorting/headers/radixPrioritySort.h
@@ -1,6 +1,29 @@
 #ifndef RADIX_PRIOR
 #define RADIX_PRIOR
 
+typedef struct Record {
+    char* fName;
+    char* lName;
+    int age;
+} Record;
+
+typedef struct RecordQueue {
+    struct RecordQueue* next;
+    struct RecordQueue* prev;
+    Record* value;
+} RecordQueue;
+
+
+Record* newRecord(char fName[], char lName[], int age);
+void deleteRecord(Record* record);
+void printRecord(Record* record);
+
+RecordQueue* newRecordQueue(Record* value);
+void deleteRecordQueue(RecordQueue** head, RecordQueue** tail);
+void enqueue(RecordQueue** head, RecordQueue** tail, Record* value);
+void printRecordQueue(RecordQueue* head);
+
+
 int testRadixPrioritySort();
 
 #endif
--- sorting/src/radixPrioritySort.c
+++ sorting/src/radixPrioritySort.c
@@ -1,5 +1,44 @@
 #include "radixPrioritySort.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+
 
 int testRadixPrioritySort(){
+    Record* me = newRecord("Matt", "Crow", 22);
+    printRecord(me);
+    deleteRecord(me);
+    me = 0;
     return 0;
+}
+
+Record* newRecord(char fName[], char lName[], int age){
+    Record* newRec = (Record*)malloc(sizeof(Record));
+    newRec->fName = fName;
+    newRec->lName = lName;
+    newRec->age = age;
+    return newRec;
+}
+void deleteRecord(Record* record){
+    free(record);
+}
+void printRecord(Record* record){
+    if(record){
+        printf("%s, %s (age %d)", record->lName, record->fName, record->age);
+    } else {
+        printf("%s", "Record is null");
+    }
+}
+
+RecordQueue* newRecordQueue(Record* value){
+    return 0;
+}
+void deleteRecordQueue(RecordQueue** head, RecordQueue** tail){
+
+}
+void enqueue(RecordQueue** head, RecordQueue** tail, Record* value){
+
+}
+void printRecordQueue(RecordQueue* head){
+
 }
