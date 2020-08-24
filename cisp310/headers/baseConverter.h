26276dd1b6629eed9c2fd8cd6d215b7cbe597c64
IronHeart7334 <greengrappler12@gmail.com>
2020-08-24 11:00:21 -0700

    Has issues with large digit counts. I'll see if I can get it to work with smaller numbers first

--- cisp310/headers/baseConverter.h
+++ cisp310/headers/baseConverter.h
@@ -2,6 +2,20 @@
 #define BASE_CONVERTER
 
 /*
+Returns the integer
+value of c when it
+is interprated as a
+hexadecimal digit
+*/
+int intVal(char c);
+
+/*
+Converts the given string ip (written in
+base fromBase) to a decimal integer.
+*/
+int calcIntValue(char ip[], int fromBase);
+
+/*
 Converts the given number string from the first base
 to the second. Note the returned value is allocated on
 the heap, and must therefore be manually freed.
--- cisp310/src/baseConverter.c
+++ cisp310/src/baseConverter.c
@@ -9,12 +9,49 @@ const int MAX_BASE = sizeof(ALPHABET) / sizeof(char);
 
 int testBaseConverter(){
     printf("%s", "Testing base converter...");
-    for(int i = -1; i < 20; i++){
-        for(int j = -1; j < 20; j++){
-            free(convert(0, i, j));
+    free(convert("0000000000000000000000000000010", 2, 10));
+    return 0;
+}
+
+int intVal(char c){
+    int ret = -1;
+    for(int i = 0; i < MAX_BASE && ret == -1; i++){
+        if(ALPHABET[i] == c){
+            ret = i;
         }
     }
-    return 0;
+    return ret;
+}
+
+int calcIntValue(char ip[], int fromBase){
+    int ret = -1;
+    if(fromBase <= 1 || fromBase > MAX_BASE){
+        printf("Invalid base: %d\n", fromBase);
+    } else {
+        ret = 0;
+        // find the number of digits in the given input
+        int digits = 0;
+        unsigned int basePower = 1;
+
+        //TODO need to account for spaces in the number
+        // probably want a sanitizer
+        // currently has issues with more than 31 digits, due to integer limits
+        while(ip[digits] != '\0'){
+            digits++;
+            basePower *= fromBase;
+        }
+        basePower /= fromBase; // for example, base 10 with 2 digits should have a base power of 10, not 100
+        int intValue;
+        for(int i = 0; ip[i] != '\0'; i++){
+            intValue = intVal(ip[i]);
+            printf("Int value is %d * %d\n", intValue, basePower);
+            if(intValue != -1){
+                ret += basePower * intValue;
+                basePower /= fromBase;
+            }
+        }
+    }
+    return ret;
 }
 
 char* convert(char ip[], int fromBase, int toBase){
@@ -33,6 +70,8 @@ char* convert(char ip[], int fromBase, int toBase){
     } else {
         // yay, finally some valid input!
         printf("I can convert from base %d to base %d\n", fromBase, toBase);
+        int intValue = calcIntValue(ip, fromBase);
+        printf("%s in base %d is %d in base 10\n", ip, fromBase, intValue);
     }
     //printf("ret is %s", ret);
     return ret;
