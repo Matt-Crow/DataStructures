72455434e8c3b1ec9e9e5035aeef359cf9a7b34e
IronHeart7334 <greengrappler12@gmail.com>
2020-08-24 17:08:38 -0700

    A bit cleaner, a bit better documented, but I'm too tired to do more. What a day!

--- cisp310/src/baseConverter.c
+++ cisp310/src/baseConverter.c
@@ -26,16 +26,21 @@ int testBaseConverter(){
         //scanf("%d", &to);
         sanitized = sanitize(ip);
         op = convert(sanitized, from, to);
-        printf("%s in base %d is %s in base %d", sanitized, from, op, to);
+        printf("%s in base %d is %s in base %d\n", sanitized, from, op, to);
         if(sanitized){
             free(sanitized);
             sanitized = 0;
         }
-        free(op);
-        op = 0;
+        if(op){
+            free(op);
+            op = 0;
+        }
+
+        keepGoing = false;
+        /*
         printf("Enter yes to continue: ");
         fgets(ip, 3, stdin);
-        keepGoing = strcmp(ip, "yes") == 0; // doesn't work, likely because of newline character
+        keepGoing = strcmp(ip, "yes") == 0; // doesn't work, likely because of newline character*/
     }
     return 0;
 }
@@ -72,7 +77,6 @@ int intVal(char c){
     return ret;
 }
 
-// this might be wrong, as I don't think I'm returning in base 10 all the time
 int calcIntValue(char ip[], int fromBase){
     int ret = -1;
     if(fromBase <= 1 || fromBase > MAX_BASE){
@@ -92,7 +96,7 @@ int calcIntValue(char ip[], int fromBase){
         int intValue;
         for(int i = 0; ip[i] != '\0'; i++){
             intValue = intVal(ip[i]);
-            printf("Int value is %d * %d\n", intValue, basePower);
+            //printf("Int value is %d * %d\n", intValue, basePower);
             if(intValue != -1){
                 ret += basePower * intValue;
                 basePower /= fromBase;
@@ -104,7 +108,7 @@ int calcIntValue(char ip[], int fromBase){
 
 char* convert(char ip[], int fromBase, int toBase){
     char* ret = 0;
-
+    // may want to move this somewhere else
     if(fromBase <= 1){
         printf("Provided fromBase %d is too low: must be greater than 1\n", fromBase);
     } else if(fromBase > MAX_BASE){
@@ -115,29 +119,30 @@ char* convert(char ip[], int fromBase, int toBase){
         printf("Provided toBase %d is to high: must be at most %d\n", toBase, MAX_BASE);
     } else {
         // yay, finally some valid input!
+
+        // create the number string to return, padded with 0s
         ret = (char*)malloc(sizeof(char) * (MAX_DIGITS + 1));
         memset(ret, ALPHABET[0], MAX_DIGITS);
         ret[MAX_DIGITS] = '\0';
-        printf("I can convert from base %d to base %d\n", fromBase, toBase);
+
+        // first, convert the input number string to a decimal integer so we can do division and other opertations
         int intValue = calcIntValue(ip, fromBase);
         printf("%s in base %d is %d in base 10\n", ip, fromBase, intValue);
+
+        // calculate the digits of the new number string in the new base
         long long maxDigitValue = (long)pow(toBase, MAX_DIGITS - 1); //the largest value of a single digit of a number in base toBase
-        printf("%d^(%d-1) = %lld\n", toBase, MAX_DIGITS, maxDigitValue);
         int count; // number of maxDigitValues in intValue
         for(int place = 0; place < MAX_DIGITS; place++){
             if(maxDigitValue < 0){
-                maxDigitValue = -maxDigitValue;
+                maxDigitValue = -maxDigitValue; // I was having issues with the sign flipping, probably due to integer overflow,
+                // but that was when I had another bug, so I'm not sure if this is needed anymore
             }
-            count = intValue / maxDigitValue;
-            printf("Place is %d\n", place);
-            ret[place] = ALPHABET[count];
-            printf("Ret is %s\n", ret);
-            intValue -= count * maxDigitValue;
-            printf("Int value is %d\n", intValue);
-            maxDigitValue /= toBase;
-            printf("maxDigitValue is %lld\n", maxDigitValue);
+            count = intValue / maxDigitValue; // "how many (toBase)^(MAX_DIGITS - 1 - place) are there in intValue?"
+            ret[place] = ALPHABET[count]; // I don't think this will pose a problem, so long as nobody enters some number like 16^33
+            intValue -= count * maxDigitValue; // there are [count] maxDigitValues in intValue, so this is (intValue / maxDigitValue) * maxDigitValue
+            // which gives the whole number of maxDigitValues in intValue.
+            maxDigitValue /= toBase; // move to the next digit of the output base number system.
         }
     }
-    printf("ret is %s\n", ret);
     return ret;
 }
