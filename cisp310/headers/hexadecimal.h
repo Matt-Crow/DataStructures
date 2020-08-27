4132b3bac1c5c21ccf128472f217db2ebf4b8e9f
IronHeart7334 <greengrappler12@gmail.com>
2020-08-27 10:11:42 -0700

    Cool! Hex parser looks like it works!

--- cisp310/headers/hexadecimal.h
+++ cisp310/headers/hexadecimal.h
@@ -3,8 +3,11 @@
 
 extern const int HEX_LEN;
 extern const char HEX_ALPHABET[];
+extern const int HEX_ALPHABET_SIZE;
 
+int hexIdx(char c);
 char* newHex();
+char* toHexStr(char* str);
 int deleteHex(char** hexStr);
 
 int testHex();
--- cisp310/src/hexadecimal.c
+++ cisp310/src/hexadecimal.c
@@ -1,6 +1,7 @@
 #include "hexadecimal.h"
 #include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
 #include<stdio.h>
 
 const int HEX_LEN = 8; // need to calculate this based on max integer and system archetecture
@@ -10,11 +11,41 @@ const char HEX_ALPHABET[] = {
     '8', '9', 'a', 'b',
     'c', 'd', 'e', 'f'
 }; // 0-15 in hex
+const int HEX_ALPHABET_SIZE = sizeof(HEX_ALPHABET) / sizeof(char);
 
+int hexIdx(char c){
+    int ret = -1;
+    c = tolower(c);
+    for(int i = 0; i < HEX_ALPHABET_SIZE && ret == -1; i++){
+        if(c == HEX_ALPHABET[i]){
+            ret = i;
+        }
+    }
+    return ret;
+}
 char* newHex(){
     char* ret = (char*)malloc(sizeof(char) * (HEX_LEN + 1));
     memset(ret, HEX_ALPHABET[0], HEX_LEN);
     ret[HEX_LEN] = '\0';
+    return ret;
+}
+char* toHexStr(char* str){
+    char* ret = newHex();
+    int rawStrLen = strlen(str);
+    printf("%s is %d long\n", str, rawStrLen);
+    int rawStrIdx = rawStrLen - 1; // start at the end of the old string
+    int retStrIdx = HEX_LEN - 1; // and at the back of the new string
+    int hexI;
+    while(rawStrIdx >= 0 && retStrIdx >= 0){
+        hexI = hexIdx(str[rawStrIdx]);
+        if(hexI == -1){
+            printf("Warning: Invalid hex character '%c'\n", str[rawStrIdx]);
+        } else {
+            ret[retStrIdx] = HEX_ALPHABET[hexI];
+            retStrIdx--;
+        }
+        rawStrIdx--;
+    }
     return ret;
 }
 int deleteHex(char** hexStr){
@@ -27,9 +58,18 @@ int deleteHex(char** hexStr){
 }
 
 int testHex(){
-    char* hex = newHex();
-    printf("Hex is %s\n", hex);
-    deleteHex(&hex);
-    printf("Is hex null? %s\n", (hex) ? "No." : "Yes.");
+    char* strs[] = {
+        "abcdef00",
+        "f",
+        "I am not hex :("
+    };
+
+    char* hex = 0;
+
+    for(int i = 0; i < 3; i++){
+        hex = toHexStr(strs[i]);
+        printf("Hex is %s\n", hex);
+        deleteHex(&hex);
+    }
     return 0;
 }
