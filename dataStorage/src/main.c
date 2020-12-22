86b16b9a7a89dd1af07d0d224335bdcb9934246b
IronHeart7334 <greengrappler12@gmail.com>
2020-12-22 12:52:09 -0800

    Much cleaner! I'll need to implement menu stuff into the other files though, as well as finding more code I can move into shared functions

--- dataStorage/src/main.c
+++ dataStorage/src/main.c
@@ -8,42 +8,19 @@
 #include "heap.h"
 
 int main(){
-
     MenuOption* options[] = {
-        newMenuOption("test stack", testStack)
+        newMenuOption("test stack", testStack),
+        newMenuOption("test queue", testQueue),
+        newMenuOption("test linked list", testLinkedList),
+        newMenuOption("test binary tree", testBinaryTree),
+        newMenuOption("test hash array", testHashArray),
+        newMenuOption("test heap", testHeap)
     };
-    doMenu(options, 1);
+    int numOptions = sizeof(options) / sizeof(MenuOption*);
 
-    int ip = 0;
-    do{
-        printf("%s", "1: test stack\n");
-        printf("%s", "2: test queue\n");
-        printf("%s", "3: test linked list\n");
-        printf("%s", "4: test binary tree\n");
-        printf("%s", "5: test hash array\n");
-        printf("%s", "6: test heap\n");
-        printf("%s", "-1: Quit\n");
-        printf("%s", "Please choose an option: ");
-        scanf("%d", &ip);
-        switch(ip){
-            case 1:
-                testStack();
-                break;
-            case 2:
-                testQueue();
-                break;
-            case 3:
-                testLinkedList();
-                break;
-            case 4:
-                testBinaryTree();
-                break;
-            case 5:
-                testHashArray();
-                break;
-            case 6:
-                testHeap();
-                break;
-        }
-    } while(ip != -1);
+    doMenu(options, numOptions);
+
+    for(int i = 0; i < numOptions; i++){
+        freeMenuOption(&(options[i]));
+    }
 }
