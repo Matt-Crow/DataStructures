23d844c5385cbd4cdf6ec8eeed5d21bcf3c2f7fa
Matt Crow <greengrappler12@gmail.com>
2021-02-20 12:37:42 -0800

    starting work on Java data structures

---
+++ JavaDataStructures/src/main/java/dataStructures/Main.java
@@ -0,0 +1,11 @@
+package dataStructures;
+
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Works");
+    }
+}
