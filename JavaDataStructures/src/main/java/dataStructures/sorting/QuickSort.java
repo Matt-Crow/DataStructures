e742b451c4aa2a8a4c532ce65887add34b5d441e
Matt Crow <greengrappler12@gmail.com>
2021-04-21 09:49:10 -0700

    feels like quicksort is almost done, I think its just my recursive calls that are wrong. Maybe I need to use an inclusive end index?

--- JavaDataStructures/src/main/java/dataStructures/sorting/QuickSort.java
+++ JavaDataStructures/src/main/java/dataStructures/sorting/QuickSort.java
@@ -44,46 +44,54 @@ public class QuickSort {
      * @param min minimum index - inclusive
      * @param max maximum index - exclusive
      */
-    public final void quickSort(Prioritizable[] array, int min, int max) {
+    private void quickSort(Prioritizable[] array, int min, int max) {
         if(min >= max){
             return;
         }
         // choose pivot. Can be any index within [min, max)
         int pivotIdx = medianIdx(array, min, max);
         Prioritizable pivot = array[pivotIdx];
+        
+        // move pivot to the end to make things easier
         swap(array, pivotIdx, max - 1);
+        
         // partition values smaller than pivot to its left
         // partition values greater than pivot to its right
         int i = min;
         int j = max - 2; // left of pivot
         while(i < j){
-            while(i < max - 2 && array[i].getPriority() < pivot.getPriority()){
+            while(i < max - 2 && array[i].getPriority() <= pivot.getPriority()){
                 i++;
             } // by now, i points to a problematic value
-            while(j > min && array[j].getPriority() > pivot.getPriority()){
+            while(j > min && array[j].getPriority() >= pivot.getPriority()){
                 j--;
             } // by now, both i and j point to problematic values
-            
             if(i < j){
                 // a[i] is greater than pivot, a[j] is lesser, so swap them 
                 swap(array, i, j);
                 i++;
                 j--;
-            } else {
+            }
+            // don't do else here
+            if(i >= j){
                 // array[i] is now greater than the pivot
-                swap(array, pivotIdx, i);
+                swap(array, max - 1, i);
                 // pivot is now in its proper place
-            }   
+            }
         }          
         
         
         System.out.printf("Pivot %d Index %d is set: %s\n", pivot.getPriority(), i, Arrays.toString(array));
         
-        
+        System.out.printf("[%d, %d) U [%d,%d)\n", min, i, i + 1, max);
         // call quicksort on the left partition
         quickSort(array, min, i);
         // call quicksort on the right partition
         quickSort(array, i + 1, max);
+    }
+    
+    public final void quickSort(Prioritizable[] array){
+        quickSort(array, 0, array.length);
     }
     
     public static void main(String[] args){
@@ -94,7 +102,7 @@ public class QuickSort {
         }
         
         System.out.println(Arrays.toString(unsorted));
-        new QuickSort().quickSort(unsorted, 0, unsorted.length);
+        new QuickSort().quickSort(unsorted);
         System.out.println(Arrays.toString(unsorted));
     }
 }
