3f62020838c07dd17efada00a22e16d9dd3f1e59
Matt Crow <greengrappler12@gmail.com>
2021-05-20 09:33:52 -0700

    done with quick sort

--- JavaDataStructures/src/main/java/dataStructures/sorting/QuickSorter.java
+++ JavaDataStructures/src/main/java/dataStructures/sorting/QuickSorter.java
@@ -6,13 +6,85 @@ import java.util.Arrays;
 import java.util.Random;
 
 /**
- *
- * @author Matt
+ *  The general idea of quicksort:
+ *       Divide and conquer the process of sorting an array by putting a single
+ *      element in its place, then dividing the problem into sub-arrays around
+ *      that pivot element.
+ * 
+ * @author Matt Crow
  */
-public class QuickSort {
+public class QuickSorter {
     
-    public QuickSort(){
+    public final void quickSort(Prioritizable[] array){
+        quickSort(array, 0, array.length);
+    }
+    
+    /**
+     * Sort the array in ascending order
+     * @param array
+     * @param min minimum index - inclusive
+     * @param max maximum index - exclusive
+     */
+    private void quickSort(Prioritizable[] array, int min, int max) {
+        if(max - min <= 1){ // don't sort one-element sets
+            return;
+        }
+        // choose pivot. Can be any index within [min, max)
+        // but the index of the median value of a[min], a[mid], a[max - 1] is the best
+        int pivotIdx = medianIdx(array, min, max - 1);
+        Prioritizable pivot = array[pivotIdx];
         
+        // move pivot to the end to make things easier
+        swap(array, pivotIdx, max - 1);
+        
+        // partition values smaller than pivot to its left
+        // partition values greater than pivot to its right
+        int i = min;
+        int j = max - 2; // left of pivot
+        while(i <= j){ // exit when i and j cross. VERY IMPORTANT: i can equal j
+            // don't move i or j if they point to same priority as the pivot
+            //                            do not use <= here! a[i] = p is problematic
+            while(i <= max && array[i].getPriority() < pivot.getPriority()){
+                i++;
+            } 
+            /*
+            by now, i points to a problematic value or the pivot. If it points to
+            the pivot, that means the pivot is the largest value in
+            this portion of the array.
+            */
+            
+            // needs the ">= min", not "> min"
+            //                            do not use >= here!
+            while(j >= min && array[j].getPriority() > pivot.getPriority()){
+                j--;
+            }
+            /*
+            by now, both i and j point to problematic values, or j has fallen 
+            off the left end of the array. If j == min - 1, that means there are
+            no values less than the pivot
+            */
+            if(i <= j){
+                // a[i] is greater than pivot, a[j] is lesser, so swap them 
+                swap(array, i, j);
+                i++;
+                j--;
+            }
+        }          
+        // array[i] is now greater than the pivot
+        swap(array, max - 1, i);
+        // pivot is now in its proper place
+        
+        
+        /*
+        System.out.printf("[%d, %d)\n", min, max);
+        System.out.printf("Pivot %d Index %d is set: %s\n", pivot.getPriority(), i, Arrays.toString(array));
+        System.out.printf("[%d, %d) U [%d,%d)\n\n", min, i, i + 1, max);
+        */
+        
+        // call quicksort on the left partition
+        quickSort(array, min, i);
+        // call quicksort on the right partition
+        quickSort(array, i + 1, max);
     }
     
     /**
@@ -51,87 +123,16 @@ public class QuickSort {
         array[b] = temp;
     }
     
-    /**
-     * Sort the array in ascending order
-     * @param array
-     * @param min minimum index - inclusive
-     * @param max maximum index - exclusive
-     */
-    private void quickSort(Prioritizable[] array, int min, int max) {
-        if(max - min <= 1){ // don't sort one-element sets
-            return;
-        }
-        // choose pivot. Can be any index within [min, max)
-        int pivotIdx = medianIdx(array, min, max - 1);
-        Prioritizable pivot = array[pivotIdx];
-        
-        // move pivot to the end to make things easier
-        swap(array, pivotIdx, max - 1);
-        
-        // partition values smaller than pivot to its left
-        // partition values greater than pivot to its right
-        int i = min;
-        int j = max - 2; // left of pivot
-        while(i <= j){ // exit when i and j cross. VERY IMPORTANT: i can equal j
-            // don't move i or j if they point to same priority as the pivot
-            //                            do not use <= here! a[i] = p is problematic
-            while(i <= max && array[i].getPriority() < pivot.getPriority()){
-                i++;
-            } 
-            /*
-            by now, i points to a problematic value or the pivot. If it points to
-            the pivot, that means the pivot is the pivot is the largest value in
-            this portion of the array.
-            */
-            
-            // needs the ">= min", not "> min"
-            //                            do not use >= here!
-            while(j >= min && array[j].getPriority() > pivot.getPriority()){
-                j--;
-            }
-            /*
-            by now, both i and j point to problematic values, or j has fallen 
-            off the left end of the array. If j == min - 1, that means there are
-            no values less than the pivot
-            */
-            if(i <= j){
-                // a[i] is greater than pivot, a[j] is lesser, so swap them 
-                swap(array, i, j);
-                i++;
-                j--;
-            }
-        }          
-        // array[i] is now greater than the pivot
-        swap(array, max - 1, i);
-        // pivot is now in its proper place
-        
-        
-        /*
-        System.out.printf("[%d, %d)\n", min, max);
-        System.out.printf("Pivot %d Index %d is set: %s\n", pivot.getPriority(), i, Arrays.toString(array));
-        System.out.printf("[%d, %d) U [%d,%d)\n\n", min, i, i + 1, max);
-        */
-        
-        // call quicksort on the left partition
-        quickSort(array, min, i);
-        // call quicksort on the right partition
-        quickSort(array, i + 1, max);
-    }
-    
-    public final void quickSort(Prioritizable[] array){
-        quickSort(array, 0, array.length);
-    }
-    
     public static void main(String[] args){
         int count = 100;
         PrioritizableInteger[] unsorted = new PrioritizableInteger[count];
         Random rng = new Random();
         for(int i = 0; i < unsorted.length; i++){
-            unsorted[i] = new PrioritizableInteger(rng.nextInt(5));
+            unsorted[i] = new PrioritizableInteger(rng.nextInt(100));
         }
         
         System.out.println(Arrays.toString(unsorted));
-        new QuickSort().quickSort(unsorted);
+        new QuickSorter().quickSort(unsorted);
         System.out.println(Arrays.toString(unsorted));
     }
 }
