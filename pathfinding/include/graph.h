f93671f097204fe8f2ea4cbfdd08a5a2bb6807ca
Matt Crow <greengrappler12@gmail.com>
2021-05-24 15:21:16 -0700

    Got started with graph.h, but having trouble focusing on this

---
+++ pathfinding/include/graph.h
@@ -0,0 +1,27 @@
+#ifndef GRAPH
+#define GRAPH
+
+typedef struct Vertex {
+    int id;
+    int x;
+    int y;
+} Vertex;
+
+typedef struct AdjacencyMatrix {
+    int** weights;
+    int size;
+} AdjacencyMatrix;
+
+typedef struct Graph {
+    Vertex** vertices; // todo resizable array of each
+    AdjacencyMatrix adjMatrix;
+    int nextId;
+} Graph; // is there some way of moving the declaration to another file?
+
+Graph* newGraph(int numVertices);
+void freeGraph(Graph** g);
+
+Vertex* createVertex(Graph* g, int x, int y);
+void createEdge(Graph* g, Vertex* v1, Vertex* v2, int weight);
+
+#endif
